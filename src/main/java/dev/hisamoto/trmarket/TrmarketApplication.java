9c2ac48f3bb0a69c1d0038389d58e456d6cd0bc4
M-Hisamoto <hisamoto.331@gmail.com>
2026-04-13 18:05:37 -0300

    First commit

---
+++ src/main/java/dev/hisamoto/trmarket/TrmarketApplication.java
@@ -0,0 +1,13 @@
+package dev.hisamoto.trmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hisamoto/trmarket/TrmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hisamoto.trmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
