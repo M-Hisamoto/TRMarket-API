e65b587c642e4c1a2ec800c1ce54cbd5c3ffe754
Matheus Hisamoto <hisamoto.331@gmail.com>
2026-05-26 00:32:18 -0300

    fix: Update CORS with correct vercel URL

--- src/main/java/dev/hisamoto/trmarket/config/SecurityConfig.java
+++ src/main/java/dev/hisamoto/trmarket/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://trmarket-frontend.vercel.app"
+                "https://tr-market-frontend.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
